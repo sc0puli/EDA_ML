ee9b5fbd8bd2c149a2b2750a224a682ce626c88d
sc0puli <tamirb2002@mail.ru>
2025-04-09 00:18:02 +0300

    rough completion lw2 task

--- Lab_2/pool.py
+++ Lab_2/pool.py
@@ -4,6 +4,8 @@ from multiprocessing import Pool
 import itertools as it
 import os
 import sys
+import json
+
 
 from timer import Time
 from distance.levenshtein import levenshtein
@@ -15,8 +17,18 @@ article_names_path = os.path.join(os.path.dirname(__file__), 'data', 'wiki title
 
 # сравнение запроса (word) с названием статьи (ref)
 def mapper(word, ref):
-    return ref, levenshtein(word, ref, lowercase=True)
+    # Проверяем разность длин строк
+    if abs(len(word) - len(ref)) > 2:
+        return ref, 10
 
+    # Сравниваем наборы букв
+    word_set = set(word.lower())
+    ref_set = set(ref.lower())
+    if len(word_set - ref_set) + len(ref_set - word_set) > 2:
+        return ref, 10
+
+    # Если оба условия не выполнены, вычисляем редакционное расстояние
+    return ref, levenshtein(word, ref, lowercase=True)
 
 # выбор лучшего результата из двух
 def reducer(current_ref, new_ref):
@@ -39,6 +51,10 @@ def chunk_processer(word, refs):
     reduced = reduce(reducer, mapped)
     return reduced
 
+# Загрузка данных из файла `words in titles.json`
+file_path = os.path.join(os.path.dirname(__file__), 'data', 'words in titles.json')
+with open(file_path, 'r') as file:
+    words_in_titles = json.load(file)
 
 # основная часть скрипта
 # Pool должен использоваться здесь
@@ -49,7 +65,7 @@ if __name__ == '__main__':
         article_names = [line.strip() for line in f]
 
     # задаем слово для поиска или читаем его из командной строки (если передан аргумент)
-    word = 'WoW'
+    word = 'AAAA'
     if len(sys.argv) > 1:
         word = sys.argv[1]
 
@@ -58,12 +74,21 @@ if __name__ == '__main__':
     # ищем ближайшее совпадение последовательно
     # при последующих запусках эту часть лучше закомментировать, чтобы не тратить время
     with Time('Последовательные вычисления'):
-        mapped = map(mapper, it.repeat(word), article_names[:len(article_names)])
+        all_words = list(words_in_titles.keys())
+        mapped = map(mapper, it.repeat(word), all_words)
         reduced = reduce(reducer, mapped)
     print(reduced)
 
     # Time считает время выполнения следующего блока и выводит его с комментарием, переданным в качестве аргумента
     # постарался сделать его похожим на директиву %time в Jupyter, это удобнее, чем модуль timeit
+
+    if reduced[0] in words_in_titles:
+        # Получаем названия статей, содержащие ближайшее слово
+        article_indices = words_in_titles[reduced[0]]
+        matching_articles = [article_names[i] for i in article_indices if i < len(article_names)]
+        print(f"Статьи, содержащие ближайшее слово: {matching_articles}")
+    else:
+        print("Ближайшее слово не найдено в словаре.")
 
     N = 32
 
@@ -75,3 +100,11 @@ if __name__ == '__main__':
             mapped = pool.starmap(chunk_processer, zip(it.repeat(word), name_chunks))
             reduced = reduce(reducer, mapped)
     print(reduced)
+
+    if reduced[0] in words_in_titles:
+        # Получаем названия статей, содержащие ближайшее слово (параллельный поиск)
+        article_indices_parallel = words_in_titles[reduced[0]]
+        matching_articles_parallel = [article_names[i] for i in article_indices_parallel if i < len(article_names)]
+        print(f"Статьи, содержащие ближайшее слово (параллельный поиск): {matching_articles_parallel}")
+    else:
+        print("Ближайшее слово не найдено в словаре (параллельный поиск).")
